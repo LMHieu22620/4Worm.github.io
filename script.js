0ef254e76ae663924951e76aae2b8ec25776bc5a
minhhieu <minhhieu207819@gmail.com>
2022-05-20 15:56:15 +0700

    eidt index

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ function showDivs(n) {
     }
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+      
     }
     x[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " w3-opacity-off";
